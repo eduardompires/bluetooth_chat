9c7da4b1c61312bfb7101e2bd4a98519c98e0e2a
Alexsander Amaral da Hora Rocha <alexsander.dahora@ge.com>
2016-11-21 14:34:24 -0200

    Sectioned Recycler view in pairing fragment

--- app/src/main/java/com/example/eduardopires/bluetooth_chat/PairingAdapter.java
+++ app/src/main/java/com/example/eduardopires/bluetooth_chat/PairingAdapter.java
@@ -1,37 +1,81 @@
 package com.example.eduardopires.bluetooth_chat;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.afollestad.sectionedrecyclerview.SectionedRecyclerViewAdapter;
+import com.truizlop.sectionedrecyclerview.SimpleSectionedAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 212571132 on 10/31/16.
  */
 
-public class PairingAdapter extends SectionedRecyclerViewAdapter {
+public class PairingAdapter extends SimpleSectionedAdapter<PairingAdapter.ItemViewHolder>{
+
+    private List<String> bondedDevices = new ArrayList<String>();
+    private List<String> availableDevices = new ArrayList<String>();
+    private Context context;
+
+    public PairingAdapter(Context context, List<String> bondedDevices, List<String> availableDevices) {
+        this.context = context;
+        this.bondedDevices = bondedDevices;
+        this.availableDevices = availableDevices;
+    }
+
     @Override
-    public int getSectionCount() {
+    protected String getSectionHeaderTitle(int section) {
+        return section == 0 ? "Dispositivos pareados" : "Dispositivos disponíveis";
+    }
+
+    @Override
+    protected int getSectionCount() {
+        return 2;
+    }
+
+    @Override
+    protected int getItemCountForSection(int section) {
+        switch (section) {
+            case 0:
+                return bondedDevices.size();
+            case 1:
+                return availableDevices.size();
+        }
         return 0;
     }
 
     @Override
-    public int getItemCount(int section) {
-        return 0;
+    protected ItemViewHolder onCreateItemViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_pairing_list, parent, false);
+        return new ItemViewHolder(view);
     }
 
     @Override
-    public void onBindHeaderViewHolder(RecyclerView.ViewHolder holder, int section) {
+    protected void onBindItemViewHolder(ItemViewHolder holder, int section, int position) {
+        String deviceName = "";
+        switch (section) {
+            case 0:
+                deviceName = bondedDevices.get(position);
+                break;
+            case 1:
+                deviceName = availableDevices.get(position);
+                break;
+        }
 
+        holder.textView.setText(deviceName);
     }
 
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int section, int relativePosition, int absolutePosition) {
 
-    }
-
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    protected static class ItemViewHolder extends RecyclerView.ViewHolder {
+        private TextView textView;
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.item_text_name);
+        }
     }
 }
--- app/src/main/java/com/example/eduardopires/bluetooth_chat/PairingFragment.java
+++ app/src/main/java/com/example/eduardopires/bluetooth_chat/PairingFragment.java
@@ -3,13 +3,18 @@ package com.example.eduardopires.bluetooth_chat;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SimpleAdapter;
 
 import com.example.eduardopires.bluetooth_chat.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +24,9 @@ public class PairingFragment extends Fragment {
     private RecyclerView recyclerView;
     private PairingAdapter adapter;
 
+    private List<String> bondedDevicesName = new ArrayList<String>();
+    private List<String> availableDevicesName= new ArrayList<String>();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,6 +35,9 @@ public class PairingFragment extends Fragment {
         recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL));
+
+        adapter = new PairingAdapter(getContext(), bondedDevicesName, availableDevicesName);
         recyclerView.setAdapter(adapter);
 
         return view;
